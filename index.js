3a542e61b46a324bfcbd8693aee1a35551accc78
Abhishek <abimano99@gmail.com>
2019-05-22 17:20:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,24 +11,25 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  request(
-    "https://newsapi.org/v2/top-headlines?category=technology&country=us&pageSize=10&apiKey=5220bc9d4b2b45e29c3cff7c3cdb5988",
-    (err, resp, body) => {
-      if (err) throw err;
-      let payload = JSON.parse(body);
-      let topNews = payload.articles;
-      const newsObj = topNews.map(news => {
-        return {
-          title: news.title,
-          desc: news.description,
-          imgUrl: news.urlToImage,
-          url: news.url
-        };
-      });
-      console.log(newsObj);
-      res.render("index", { newsObj: newsObj });
-    }
-  );
+//   request(
+//     "https://newsapi.org/v2/top-headlines?category=technology&country=us&pageSize=10&apiKey=5220bc9d4b2b45e29c3cff7c3cdb5988",
+//     (err, resp, body) => {
+//       if (err) throw err;
+//       let payload = JSON.parse(body);
+//       let topNews = payload.articles;
+//       const newsObj = topNews.map(news => {
+//         return {
+//           title: news.title,
+//           desc: news.description,
+//           imgUrl: news.urlToImage,
+//           url: news.url
+//         };
+//       });
+//       console.log(newsObj);
+//       res.render("index", { newsObj: newsObj });
+//     }
+//   );
+  res.send("welcome");
 });
 
 app.listen(3000, () => console.log("Sever is running at port 3000....."));
